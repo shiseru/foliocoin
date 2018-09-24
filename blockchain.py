5a46107a49969d097db372a52249622e8868ae2d
Shisei Naka <nathaniel.naka@gmail.com>
2018-09-24 21:24:22 +0900

    add function to check the correctness of the entire block

--- blockchain.py
+++ blockchain.py
@@ -23,6 +23,7 @@ class Blockchain:
                  'proof' : proof,
                  'previous_hash' : previous_hash}
         self.chain.append(block)
+
         return block
 
     def get_previous_block(self):
@@ -47,4 +48,42 @@ class Blockchain:
                 check_proof = True
             else:
                 new_proof += 1
+
         return new_proof
+
+    def hash(self, block):
+        """
+        encode the block
+        :param block: dict
+        :return: str
+        """
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        """
+        check if entire blockchain is valid
+        :param chain: list
+        :return: bool
+        """
+        previous_block = chain[0]
+        block_index = 1
+
+        while block_index < len(chain):
+
+            block = chain[block_index]
+
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+
+            if hash_operation[:4] != '0000'
+                return False
+
+            previous_block = block
+            block_index += 1
+
+        return True
